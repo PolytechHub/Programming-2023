de59d5c65df3271295ad450a9a1d2c6f4304c177
romantihiy <romantihomirov1@gmail.com>
2023-11-23 03:13:10 +0300

    Updated Lab6 in Lab9

--- Lab9/Part1-option4/Lab6/Part1-option4/app.py
+++ Lab9/Part1-option4/Lab6/Part1-option4/app.py
@@ -1,21 +1,87 @@
 import sys
-from functions import get_array, get_num, f1a, f1b, f2
-from PyQt6.QtWidgets import QFileDialog, QApplication, QWidget
+import design
+from PyQt6.QtWidgets import QMainWindow, QApplication, QMessageBox, QFileDialog
+from PyQt6.QtCore import QRegularExpression as QRegExp
+from PyQt6.QtGui import QRegularExpressionValidator as QRegExpValidator
+from functions import get_array, f1a, f1b, f2
 
-app = QApplication(sys.argv)
-window = QWidget()
-filename, _ = QFileDialog.getSaveFileName(
-    window, "Save data", "data.txt", "Text (*.txt)")
-if filename != '':
-    with open(filename, 'w') as f:
-        array = get_array()
-        a = get_num(0)
-        k1 = get_num(0, len(array) - 2)
-        k2 = get_num(k1, len(array) - 1)
-        f.write(f"Array: {array}\n")
-        f.write(f"a: {a}\n")
-        f.write(f"k1: {k1}\n")
-        f.write(f"k2: {k2}\n")
-        f.write(f"f1a: {f1a(array, a)}\n")
-        f.write(f"f1b: {f1b(array, k1, k2)}\n")
-        f.write(f"f2: {f2(array)}\n")
+class App(design.Ui_Form, QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.boxF1a.setValidator(QRegExpValidator(
+            QRegExp("^(-?\d+(\s-?\d+)*)$"), self.boxF1a))
+        self.buttonF1aGen.clicked.connect(self.generateF1aHandler)
+        self.buttonF1aEval.clicked.connect(self.resultF1aHandler)
+        self.boxF1b.setValidator(QRegExpValidator(
+            QRegExp("^(-?\d+(\s-?\d+)*)$"), self.boxF1b))
+        self.buttonF1bGen.clicked.connect(self.generateF1bHandler)
+        self.buttonF1bEval.clicked.connect(self.resultF1bHandler)
+        self.boxF2.setValidator(QRegExpValidator(
+            QRegExp("^(-?\d+(\s-?\d+)*)$"), self.boxF2))
+        self.buttonF2Gen.clicked.connect(self.generateF2Handler)
+        self.buttonF2Eval.clicked.connect(self.resultF2Handler)
+
+    def generateF1aHandler(self):
+        self.boxF1a.setText(' '.join(get_array(10, strings=True)))
+    
+    def generateF1bHandler(self):
+        self.boxF1b.setText(' '.join(get_array(10, strings=True)))
+
+    def generateF2Handler(self):
+        self.boxF2.setText(' '.join(get_array(10, strings=True)))
+
+    def resultF1aHandler(self):
+        if self.boxF1a.text() == '':
+            QMessageBox.about(self, 'Ошибка', "Первое поле не может быть пустым!")
+            return
+        result = f1a(boxRipper(self.boxF1a), self.boxF1aA.value())
+        if result == -1:
+            result = "Таких элементов не найдено"
+        filename, _ = QFileDialog.getSaveFileName(
+            self, "Save f1a", "dataf1a.txt", "Text (*.txt)")
+        if filename == '':
+            return
+        with open(filename, 'w') as f:
+            f.write(str(result))
+
+    def resultF1bHandler(self):
+        if self.boxF1b.text() == '':
+            QMessageBox.about(self, 'Ошибка', "Второе поле не может быть пустым!")
+            return
+        arr = boxRipper(self.boxF1b)
+        self.boxF1bK2.setValue(min(self.boxF1bK2.value(), len(arr)))
+        self.boxF1bK1.setValue(min(self.boxF1bK2.value(), self.boxF1bK1.value()))
+        result = f1b(arr, self.boxF1bK1.value(), self.boxF1bK2.value())
+        filename, _ = QFileDialog.getSaveFileName(
+            self, "Save f1b", "dataf1b.txt", "Text (*.txt)")
+        if filename == '':
+            return
+        with open(filename, 'w') as f:
+            f.write(str(result))
+
+    def resultF2Handler(self):
+        if self.boxF2.text() == '':
+            QMessageBox.about(self, 'Ошибка', "Третье поле не может быть пустым!")
+            return
+        result = f2(boxRipper(self.boxF2))
+        filename, _ = QFileDialog.getSaveFileName(
+            self, "Save f2", "dataf2.txt", "Text (*.txt)")
+        if filename == '':
+            return
+        with open(filename, 'w') as f:
+            f.write(str(result))
+
+def boxRipper(box):
+    if box.text() == '' or box.text() == '-inf':
+        return []
+    return list(map(int, box.text().split(' ')))
+
+def main():
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec()
+
+if __name__ == '__main__':
+    main()
--- Lab9/Part1-option4/Lab6/Part1-option4/functions.py
+++ Lab9/Part1-option4/Lab6/Part1-option4/functions.py
@@ -2,7 +2,9 @@ from collections import Counter
 from random import randint
 from math import inf
 
-def get_array(len=100, mini=-1000, maxi=1000):
+def get_array(len=100, mini=-1000, maxi=1000, strings=False):
+    if strings:
+        return [str(randint(mini, maxi)) for _ in range(len)]
     return [randint(mini, maxi) for _ in range(len)]
 
 def get_num(mini=-1000, maxi=1000):
@@ -12,6 +14,10 @@ def f1a(arr: list, a: int):
     minindex = -1
     minval = inf
     for i, val in enumerate(arr):
+        if val == 0:
+            minindex = i
+            minval = val
+            continue
         evens = 0
         product = 1
         for digit in str(abs(val)):
@@ -19,29 +25,36 @@ def f1a(arr: list, a: int):
             if digit % 2 == 0:
                 evens += 1
             product *= digit
-        if (evens == a or product % val == 0) and minval > val:
+        if (evens == a or product != 0 and val % product == 0) and minval > val:
             minindex = i
             minval = val
     return minindex
 
 def f1b(arr, k1, k2):
     def validate(n):
-        # Ищем элемент с минимальной суммой делителей, включая 1
-        # Иначе почти всегда будет 0
-        b = abs(n)
-        s = 0
-        for i in range(1, n):
+        # Ищем элементы с суммой делителей, включая 1 = самому элементу
+        # Они же «совершенные числа»
+        # Иначе почти всегда ничего не найдем
+        n = abs(n)
+        s = 1
+        i = 2
+        while i*i <= n:
             if n % i == 0:
-                s += i
+                s += i + n//i
+                if i*i == n:
+                    s -= i
+            i += 1
         return s == n
 
-    nums = []
+    summ = 0
+    cnt = 0
     for i in arr[k1:k2+1]:
         if validate(i):
-            nums.append(i)
-    if len(nums) == 0:
+            summ += i
+            cnt += 1
+    if cnt == 0:
         return 0
-    return sum(nums) / len(nums)
+    return summ / cnt
 
 def f2(arr):
     c = Counter(arr)
